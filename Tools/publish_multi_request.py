64c03d744630373cb74083e9af2bd05838f8d03c
ReboundQ3 <ReboundQ3@gmail.com>
2025-07-30 14:01:39 +0200

    Update publish_multi_request.py
    Yes i'm commiting to master because i dont want this to be PR#100
    Plus you can't stop me >:)
    
    Signed-off-by: ReboundQ3 <ReboundQ3@gmail.com>

--- Tools/publish_multi_request.py
+++ Tools/publish_multi_request.py
@@ -15,7 +15,7 @@ RELEASE_DIR = "release"
 # CONFIGURATION PARAMETERS
 # Forks should change these to publish to their own infrastructure.
 #
-ROBUST_CDN_URL = "https://upload.latestation14.space/"
+ROBUST_CDN_URL = "https://cdn.latestation14.space:8443/"
 FORK_ID = "latestation"
 
 def main():
