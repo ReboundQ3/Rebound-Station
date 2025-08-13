c93017c8f21e7bf5be383e1b31ad73f1d03d76ef
ReboundQ3 <ReboundQ3@gmail.com>
2025-08-13 12:47:34 +0200

    Last commit to master i promise

--- Tools/publish_github_artifact.py
+++ Tools/publish_github_artifact.py
@@ -14,8 +14,8 @@ VERSION = os.environ['GITHUB_SHA']
 # CONFIGURATION PARAMETERS
 # Forks should change these to publish to their own infrastructure.
 #
-ROBUST_CDN_URL = "https://wizards.cdn.spacestation14.com/"
-FORK_ID = "wizards"
+ROBUST_CDN_URL = "https://cdn.latestation14.space:8443/"
+FORK_ID = "latestation"
 
 def main():
     print("Fetching artifact URL from API...")
