faafb11004691e4f64aad4d6b697daedc4d80c29
ReboundQ3 <ReboundQ3@gmail.com>
2025-08-15 13:53:45 +0200

    fixep

--- Content.Client/_CD/Records/UI/CharacterRecordViewer.xaml.cs
+++ Content.Client/_CD/Records/UI/CharacterRecordViewer.xaml.cs
@@ -148,6 +148,7 @@ public sealed partial class CharacterRecordViewer : FancyWindow
         RecordEntryViewType.AddItem(Loc.GetString("department-Security"));
         RecordEntryViewType.AddItem(Loc.GetString("department-Medical"));
         RecordEntryViewType.AddItem(Loc.GetString("humanoid-profile-editor-cd-records-employment"));
+        RecordEntryViewType.AddItem(Loc.GetString("humanoid-profile-editor-cd-records-admin"));
         RecordEntryViewType.OnItemSelected += args =>
         {
             if (args.Id == RecordEntryViewType.SelectedId)
